df532ef30f778dfd32145434b0eac66360895f5d
dan2510 <carballo2597@gmail.com>
2025-11-08 23:02:59 -0600

    modificancion del role prisma

--- prisma/seeds/roles.ts
+++ prisma/seeds/roles.ts
@@ -7,12 +7,8 @@ export const roles = [
     nombre: RoleNombre.ADMIN,
     descripcion: "Administrador del sistema con todos los permisos",
   },
+
   // 2
-  {
-    nombre: RoleNombre.CLIENTE,
-    descripcion: "Supervisor de equipo técnico con permisos de gestión",
-  },
-  // 3
   {
     nombre: RoleNombre.TECNICO,
     descripcion: "Técnico de soporte para resolución de tickets",
